90d1c0af2ac93fcf29fbffbf764048677df6d9be
Thiago Felix <thiagofelixdasilva099@gmail.com>
2021-11-21 21:53:13 -0400

    part 1 solution

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,8 +7,16 @@ import { Container, Cart } from './styles';
 import { useCart } from '../../hooks/useCart';
 
 const Header = (): JSX.Element => {
-  // const { cart } = useCart();
-  // const cartSize = // TODO;
+  const { cart } = useCart();
+
+  const uniqueArray = cart.filter((product, index, self) =>
+    index === self.findIndex((t) => (
+      t.id === product.id
+    ))
+  )
+  console.log(uniqueArray);
+
+  const cartSize = uniqueArray.length;
 
   return (
     <Container>
@@ -20,7 +28,7 @@ const Header = (): JSX.Element => {
         <div>
           <strong>Meu carrinho</strong>
           <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
+            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
           </span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,20 +23,37 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const new_product_cart: Product = await (await api.get(`/products/${productId}`)).data;
+      new_product_cart.amount = 1;
+      setCart([...cart, new_product_cart]);
+      let new_cart: Product[] = [];
+
+      const product_cart = cart.filter(product => (product.id === productId))
+
+      if (product_cart.length > 0) {
+        product_cart[0].amount += 1;
+        new_cart = ([...cart, product_cart[0]]);
+      } else {
+        const new_product_cart: Product = await (await api.get(`/products/${productId}`)).data;
+        new_product_cart.amount = 1;
+        new_cart = ([...cart, new_product_cart]);
+      }
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(new_cart));
+      setCart(new_cart);
+
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
